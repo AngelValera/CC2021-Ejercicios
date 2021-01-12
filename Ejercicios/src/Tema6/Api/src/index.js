f24ce5c87edeb60f080e8cac092c316cf508b296
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-01-12 14:20:58 +0100

    Avanzo #6: Agrego fuentes del ejercicio 5

--- Ejercicios/src/Tema6/Api/src/index.js
+++ Ejercicios/src/Tema6/Api/src/index.js
@@ -1,26 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
+// el Servicio de Etcd3 debe estar funcionando
 const { Etcd3 } = require("etcd3");
 const client = new Etcd3();
 
 async function getPort() {
   const port = await client.get("AnimalsPort");
+  await client.delete().all();
   return port;
 }
 
 // Settings
-//app.set("port", process.env.PORT || 3000);
-app.set('json spaces', 2);
+app.set("json spaces", 2);
 
 //Middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Routes
-app.use(require('./routes/index'));
+app.use(require("./routes/index"));
 
 app.use(function (request, response) {
   response.status(400);
@@ -35,11 +36,11 @@ app.use(function (request, response) {
 //Starting the server
 (async () => {    
   let PORT = await getPort();  
-  app.set("port", PORT || process.env.PORT || 3000);    
+  app.set("port", PORT || process.env.PORT || 3000);   
 })().then(() => {
   app.listen(app.get("port"), () => {
     console.log(`Server on port ${app.get("port")}`);
   });
-});
+});  
 
 module.exports = app;
